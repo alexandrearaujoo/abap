77226f245e565664e8827bbb2c2f4aca540bd591
David Almeida <contato.davidalmeida@outlook.com>
2022-03-15 13:09:14 -0300

    Add: Header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: var(--color-primary);
+  width: 100vw;
+  height: 10vh;
+  color: var(--white);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  svg {
+    font-size: 30px;
+  }
+
+  h1 {
+    font-size: var(--fontsize-h3);
+  }
+`

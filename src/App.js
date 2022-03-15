ea225bd08f99d25f73f4d1dfbf6ffa92c5ca4478
Alexandre Araujo <alexandre01393@gmail.com>
2022-03-15 14:41:57 -0300

    feat: modalForm created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import FormModal from './components/FormModal';
 import { GlobalStyle } from './style/global';
 
 function App() {
   return (
       <>
         <GlobalStyle />
-        <FormModal />
       </>
   );
 }

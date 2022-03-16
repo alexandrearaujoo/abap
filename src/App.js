8ae5698cd2338c4ba846940fdbf67c07b4502e60
Hamart Rony <hamart23@icloud.com>
2022-03-15 22:45:08 -0300

    feat: create input component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Form } from './components/FormModal/style';
+import Header from './components/Header';
+import Input  from './components/Input';
 import { GlobalStyle } from './style/global';
 
 function App() {
   return (
       <>
         <GlobalStyle />
+        <Header/>
+        <Form>
+         <Input/>
+
+        </Form>
       </>
   );
 }
---
+++ src/components/Input/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+export const Inputs = styled.div`
+    
+    border-bottom: 2px var(--purple-1) solid;
+    position: relative;
+
+
+    input{
+        background: transparent;
+        border: none;
+        box-shadow: none;
+    }
+
+    textarea:focus, input:focus, select:focus {
+    box-shadow: 0 0 0 0;
+    border: 0 none;
+    outline: 0;
+    } 
+
+ 
+    label{
+        pointer-events: none;
+        position: absolute;
+        margin-left: -82%;
+        margin-top: 7px;
+        font-size: 15px;
+        color: var(--white);
+    }
+
+    input:focus + label{
+        margin-top: -10px;
+        font-size: 10px;
+        color:var(--purple-1);
+        transition: .2s;
+}
+
+    input:not(:placeholder-shown) + label{
+            margin-top: -10px;
+            font-size: 10px;
+            color:var(--purple-1);
+            transition: .2s;
+    }
+
+
+
+
+   
+
+
+   
+
+
+
+`

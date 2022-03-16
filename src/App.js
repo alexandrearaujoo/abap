5a821df71da71b310f5840a585746b91132a3210
Alexandre Araujo <alexandre01393@gmail.com>
2022-03-15 22:26:03 -0300

    feat: dashboard created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { GlobalStyle } from './style/global';
+import Header from './components/Header'
+import Router from './routes';
 
 function App() {
   return (
       <>
+        <Header/>
         <GlobalStyle />
+        <Router />  
       </>
   );
 }
---
+++ src/pages/DashboardAdmin/index.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../../components/Sidebar"
+
+const DashBoardADM = () => {
+    return (
+        <>
+            <Sidebar />
+        </>
+    )
+}
+
+export default DashBoardADM

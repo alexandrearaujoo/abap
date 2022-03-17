d0496869bbe54d0e084bce9f935e48d80082c5b6
Alexandre Araujo <alexandre01393@gmail.com>
2022-03-17 09:14:03 -0300

    update: add hook form

--- src/components/FormModalAssociados/index.jsx
+++ src/components/FormModalAssociados/index.jsx
@@ -1,28 +1,54 @@
 import {Form, DivInfos, DivLocal} from './style'
 import Button from '../Button'
+import {useForm} from 'react-hook-form'
+import * as yup from 'yup'
+import {yupResolver} from '@hookform/resolvers'
 
 const FormModalAssociados = () => {
+
+
+    const schema = yup.object.schema({
+        nome_completo: yup.string().required("Campo Obrigatorio"),
+        email: yup.string.email('Email invalido').required('Campo Obrigatorio'),
+        endereco: yup.string().required('Campo Obrigadotiro'),
+        password: yup.string().required('Campo Obrigatorio'),
+        numero: yup.string().required('Campo Obrigatorio'),
+        bairro: yup.string().required('Campo Obrigatorio'),
+        cidade: yup.string().required('Campo Obrigatorio'),
+        estado: yup.string().required('Campo Obrigatorio')
+    })
+
+    const {register, handleSubmit, formState: {errors}} = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
         <section>
-            <Form>
+            <Form onSubmit={handleSubmit(onSubmit)}>
                 <h2>Cadastro</h2>
                 <label>Nome completo</label>
                 <input />
                 <label>Email</label>
                 <input />
+                <label>Senha</label>
+                <input />
                 <label>Endereço</label>
                 <input />
                 <DivInfos>
                     <DivLocal>
-                        <label>CEP</label>
+                        <label>Numero</label>
                         <input />
-                        <label>Ponto de Ref</label>
+                        <label>Bairro</label>
                         <input />
                     </DivLocal>
                     <DivLocal>
-                        <label>CPF</label>
+                        <label>Cidade</label>
                         <input />
-                        <label>Telefone</label>
+                        <label>Estado</label>
                         <input />
                     </DivLocal>
                 </DivInfos>

680f0ecaced17ae7b7ed60951a4dd44e611b7c06
NatanFiuza <natan.fiuza@gmail.com>
2022-03-18 12:29:54 -0300

    feat: create provider tokenAssociado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { GlobalStyle } from './style/global';
 import Router from './routes';
-
+import  { Toaster }  from "react-hot-toast";
 function App() {
   return (
-      <>
-        <GlobalStyle />
-        <Router />  
-      </>
+    <>
+      <GlobalStyle />
+      <Toaster position="top-center" reverseOrder={false} />
+      <Router />
+    </>
   );
 }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import Associados from '../pages/Associados'
 import CadastroAssociados from '../pages/CadastroAssociados'
@@ -6,33 +6,35 @@ import GerenciarPagamentos from '../pages/GerenciarPagamentos'
 import DashBoardADM from '../pages/DashboardAdmin'
 import Solicitacoes from '../pages/Solicitacoes'
 import LoginAssociado from '../pages/LoginAssociado'
-import {AnimatePresence} from 'framer-motion'
-
+import { AnimatePresence } from 'framer-motion'
+import { TokenAssociadoProvider } from '../providers/tokenAssociado.js'
 const Router = () => {
     return (
         <AnimatePresence>
-            <Switch>
-                <Route exact path='/'>            
-                </Route>
-                <Route path='/loginAssociado'>
-                    <LoginAssociado/>
-                </Route>
-                <Route path='/dashboardadm'>
-                    <DashBoardADM />
-                </Route>    
-                <Route path='/associados'>
-                    <Associados />
-                </Route>
-                <Route path='/gerenciamento'>
-                    <GerenciarPagamentos />
-                </Route>
-                <Route path='/cadastros'>
-                    <CadastroAssociados />
-                </Route>
-                <Route path='/solicitacoes'>
-                    <Solicitacoes />
-                </Route>
-            </Switch>
+            <TokenAssociadoProvider>
+                <Switch>
+                    <Route exact path='/'>
+                    </Route>
+                    <Route path='/loginAssociado'>
+                        <LoginAssociado />
+                    </Route>
+                    <Route path='/dashboardadm'>
+                        <DashBoardADM />
+                    </Route>
+                    <Route path='/associados'>
+                        <Associados />
+                    </Route>
+                    <Route path='/gerenciamento'>
+                        <GerenciarPagamentos />
+                    </Route>
+                    <Route path='/cadastros'>
+                        <CadastroAssociados />
+                    </Route>
+                    <Route path='/solicitacoes'>
+                        <Solicitacoes />
+                    </Route>
+                </Switch>
+            </TokenAssociadoProvider>
         </AnimatePresence>
     )
 }

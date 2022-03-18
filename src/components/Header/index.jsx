fcbba8983156ff95de500b24d31eab75b08993d3
David Almeida <contato.davidalmeida@outlook.com>
2022-03-18 09:39:08 -0300

    Update: Header receive dinamic props

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,20 +3,20 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { StyledContainer } from "../../style/global";
 import { StyledHeader } from "./styles";
 
-import Logo from "../../assets/svg/logo.svg";
+import Logo from "../../assets/img/logo.png";
 
-const Header = () => {
+const Header = ({text, link}) => {
   const history = useHistory();
 
   const handleLogin = () => {
-    history.push("/login");
+    history.push(`${link}`);
   };
 
   return (
     <StyledHeader>
       <StyledContainer>
         <img width={40} src={Logo} alt="Logo" />
-        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleLogin}>{text}</button>
       </StyledContainer>
     </StyledHeader>
   );

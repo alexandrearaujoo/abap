293cb8b2346514fc7c1cc050bb40e97f55ebe644
David Almeida <contato.davidalmeida@outlook.com>
2022-03-20 10:27:14 -0300

    Add: Page DashboardAssociado

---
+++ src/components/Main/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--white);
+  width: 80vw;
+  height: 90vh;
+  margin: 0 auto;
+  
+  position: relative;
+  z-index: -1;
+`

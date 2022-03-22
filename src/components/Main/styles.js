53894499662ca9995ab6796b8df77d00a3b6e225
David Almeida <contato.davidalmeida@outlook.com>
2022-03-22 00:08:18 -0300

    Update: justify-content: center for flex-start

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledMain = styled.main`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   background-color: var(--white);
   width: 100vw;

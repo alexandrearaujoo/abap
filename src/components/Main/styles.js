3e13b455027694697f43036325b9be0b6b4edfe2
David Almeida <contato.davidalmeida@outlook.com>
2022-03-21 21:53:59 -0300

    Update: adding media query and font sizes for h2 and h3

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -6,11 +6,25 @@ export const StyledMain = styled.main`
   justify-content: center;
   align-items: center;
   background-color: var(--white);
-  width: 100%;
-  max-width: 900px;
-  height: 90vh;
+  width: 100vw;
+  height: 100vh;
   margin: 0 auto;
   position: relative;
   top: 12vh;
   z-index: 0;
+
+  h2 {
+      font-size: var(--fontsize-h2);
+      padding: 20px 0;
+  }
+
+  h3 {
+      font-size: var(--fontsize-h3);
+      padding: 20px 0;
+  }
+
+  @media (min-width: 768px) {
+    width: 80vw;
+    height: 88vh;
+  }
 `
